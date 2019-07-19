92234f3b48899abd2664bf78f7436877b50ce0bb
Fabrizio Miano <fabriziomiano@gmail.com>
2019-07-19 22:36:37 +0200

    change shebang; reformat

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
-#!/usr/bin/env bash
-virtualenv .env -p python3 && source .env/bin/activate && pip install -r requirements.txt
+#!/bin/bash
+virtualenv fbpostana -p python3 && \
+    source fbpostana/bin/activate && \
+    pip install -r requirements.txt
