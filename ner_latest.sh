31a4eb58576f8754ee6f5897ff86cf20bf0ded43
Fabrizio Miano <fabriziomiano@gmail.com>
2019-07-20 19:24:27 +0200

    Add script to run NER on latest posts

---
+++ ner_latest.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+function usage()
+{
+    echo ""
+    echo -e "\tInstructions"
+    echo ""
+    echo -e "\tsource ner_latest.sh <path/to/config-file>"
+    echo ""
+}
+
+CONFIG=$1
+if [[ -z $CONFIG ]]; then
+    echo "ERROR :: Config file not specified"
+    echo "Please specify the config-file path to use"
+    echo -e "\a"
+    usage
+else
+    echo "INFO :: Running Named-Entity Recognition using config file:" $CONFIG
+    python ./run_ner_latest.py --conf $CONFIG
+fi
