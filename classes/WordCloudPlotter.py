ada1b837a51f6b8573c731168666da85a12b9997
Fabrizio Miano <fabriziomiano@gmail.com>
2019-07-20 17:42:33 +0200

    rename class

--- classes/WordCloudPlotter.py
+++ classes/WordCloudPlotter.py
@@ -1,18 +1,13 @@
-import pandas as pd
-import seaborn as sns
-from matplotlib import pyplot as plt
 from wordcloud import WordCloud
 
 
 class Plotter(object):
-    def __init__(self, long_string, wordcount_data):
+    def __init__(self, long_string):
         """
         :param long_string:  str: Whitespace concatenation of
             all the words in a given corpus
-        :param wordcount_data: list of tuples(word, wordcount)
         """
         self.long_string = long_string
-        self.wordcount_data = wordcount_data
 
     def save_wordcloud_plot(self, path):
         """
@@ -24,7 +19,7 @@ class Plotter(object):
         wc = WordCloud(
             background_color="black",
             contour_width=3,
-            contour_color='steelblue'
+            contour_color="steelblue"
         )
         wc.generate(self.long_string)
         wc.to_file(path)
@@ -43,20 +38,3 @@ class Plotter(object):
         wc.generate(self.long_string)
         wc.to_image()
         return wc.to_image()
-
-    def save_barplot(self, n_top_words, path):
-        """
-        Save bar plot of given word count data
-
-        :param n_top_words: int number of top words to plot
-        :param path:
-        :return:
-        """
-        df = pd.DataFrame(self.wordcount_data, columns=["word", "count"])
-        plt.figure(figsize=(n_top_words, 10))
-        barplot = sns.barplot("word", "count", data=df[:n_top_words], palette="Blues_d")
-        barplot.set_title("Top {} Words".format(n_top_words))
-        plt.xticks(rotation=30)
-        plt.xlabel("Word")
-        plt.ylabel("Count", labelpad=60, rotation=0)
-        plt.savefig(path)
